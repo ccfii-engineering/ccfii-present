f76d6630a7e086141a1d0829cf96866983c7b453
alxlion <dev@alexandrelion.com>
2026-08-07 06:12:38 +0000

    Handle missing users email index

--- priv/repo/migrations/20260807052500_allow_reusing_deleted_user_emails.exs
+++ priv/repo/migrations/20260807052500_allow_reusing_deleted_user_emails.exs
@@ -2,7 +2,7 @@ defmodule Claper.Repo.Migrations.AllowReusingDeletedUserEmails do
   use Ecto.Migration
 
   def change do
-    drop unique_index(:users, [:email])
+    drop_if_exists unique_index(:users, [:email])
     create unique_index(:users, [:email], where: "deleted_at IS NULL")
   end
 end
