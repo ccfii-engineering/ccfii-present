59b298023899a319d5346fccf57088bade637f45
paotea <paolo@digitzs.com>
2026-08-12 13:23:50 +0800

    feat: add CCFII Present visual theme

--- test/claper_web/components/brand_components_test.exs
+++ test/claper_web/components/brand_components_test.exs
@@ -5,6 +5,21 @@ defmodule ClaperWeb.BrandComponentsTest do
 
   alias ClaperWeb.BrandComponents
 
+  @png_signature <<137, 80, 78, 71, 13, 10, 26, 10>>
+  @runtime_brand_assets ["ccfii-present-logo.png", "ccfii-present-mark.png"]
+
+  test "committed runtime brand image assets exist and are PNGs" do
+    for filename <- @runtime_brand_assets do
+      path = Path.join([File.cwd!(), "priv", "static", "images", filename])
+
+      assert File.regular?(path), "expected #{filename} to be committed at #{path}"
+      assert File.stat!(path).size > 0, "expected #{filename} to be non-empty"
+
+      assert File.read!(path) |> binary_part(0, 8) == @png_signature,
+             "expected #{filename} to begin with the PNG signature"
+    end
+  end
+
   test "renders the accessible full lockup" do
     html = render_component(&BrandComponents.logo/1, %{variant: :full, class: "h-10"})
     assert html =~ ~s(src="/images/ccfii-present-logo.png")
